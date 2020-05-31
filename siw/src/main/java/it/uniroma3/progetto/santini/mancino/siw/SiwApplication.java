8d16c55cb19fcc10875a79b5f316481ff4aad0ed
Matteo Santini <santinimatteo31@gmail.com>
2020-05-31 17:05:45 +0200

    inizializzazione pt.2

---
+++ siw/src/main/java/it/uniroma3/progetto/santini/mancino/siw/SiwApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.progetto.santini.mancino.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiwApplication.class, args);
+	}
+
+}

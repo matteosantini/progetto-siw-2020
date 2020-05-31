40c31cf8353b4f25ae2da829bf2540df61ba3faf
Matteo Santini <santinimatteo31@gmail.com>
2020-05-31 16:56:52 +0200

    inizializzazione

---
+++ siw-2020/siw/src/test/java/it/uniroma3/progetto/santini/mancino/siw/SiwApplicationTests.java
@@ -0,0 +1,13 @@
+package it.uniroma3.progetto.santini.mancino.siw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

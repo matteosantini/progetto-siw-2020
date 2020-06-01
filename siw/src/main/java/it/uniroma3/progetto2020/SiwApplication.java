76973e206f94c3d39856768ffe0d6fec9c117ef6
Matteo Santini <santinimatteo31@gmail.com>
2020-06-01 13:34:18 +0200

    inizializzazione esempio hello world

--- siw/src/main/java/it/uniroma3/progetto2020/SiwApplication.java
+++ siw/src/main/java/it/uniroma3/progetto2020/SiwApplication.java
@@ -1,13 +1,13 @@
-package it.uniroma3.progetto2020;
+package it.uniroma3.progetto2020; 
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SiwApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SiwApplication.class, args);
 	}
 
-}
+} 
+ 
---
+++ siw/src/main/java/it/uniroma3/progetto2020/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package it.uniroma3.progetto2020.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+	
+	@RequestMapping(value= "/hello", method = RequestMethod.GET)
+	public String sayHello() {
+		return "Hello World!";
+	}
+	
+}

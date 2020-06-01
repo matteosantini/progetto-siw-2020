035a1965e209cbe02f5228d8476aaaba235a3488
Matteo Santini <santinimatteo31@gmail.com>
2020-06-01 15:35:18 +0200

    fx

--- siw/src/main/java/it/uniroma3/progetto2020/controller/HelloWorldController.java
+++ siw/src/main/java/it/uniroma3/progetto2020/controller/HelloWorldController.java
@@ -1,15 +1,16 @@
 package it.uniroma3.progetto2020.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloWorldController {
 	
-	@RequestMapping(value= "/hello", method = RequestMethod.GET)
-	public String sayHello() {
-		return "Hello World!";
+	@RequestMapping("/")
+	public String index() {
+		return "index";
 	}
 	
 }
---
+++ siw/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,8 @@
+    <html xmlns:th="http://www.thymeleaf.org">
+    <head>
+        <title>Benvenuto</title>
+    </head>
+    <body>
+        <div align="center">Hello World!</div>
+    </body>
+</html>

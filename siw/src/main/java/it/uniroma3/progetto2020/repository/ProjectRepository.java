f45893dbea12a994b710a597b47804ec13e62121
Matteo Santini <santinimatteo31@gmail.com>
2020-06-01 17:31:57 +0200

    project repository

--- siw/src/main/java/it/uniroma3/progetto2020/repository/ProjectRepository.java
+++ siw/src/main/java/it/uniroma3/progetto2020/repository/ProjectRepository.java
@@ -1,5 +1,28 @@
 package it.uniroma3.progetto2020.repository;
 
-public interface ProjectRepository {
+import java.util.Date;
+import java.util.List;
 
-}
+import it.uniroma3.progetto2020.model.Progetto;
+import it.uniroma3.progetto2020.model.Task;
+import it.uniroma3.progetto2020.model.Utente;
+
+public interface ProjectRepository {
+	
+	public void creaProgetto(String nome, Date datadiinizio, List<Task> taskProgetto, Utente proprietario,
+			List<Utente> utentiautorizzati);
+	
+	public List<Progetto> getProgettiByUtente(Long id);
+	
+	public List<Progetto> getProgettiCondivisi(Long id);
+	
+	public void editProgetto(Long id, String nome, Date datadiinizio, List<Task> taskProgetto, Utente proprietario,
+			List<Utente> utentiautorizzati);
+	
+	public void deleteProgetto(Long id);
+	
+	public void aggiungiTag();
+	
+	public void condiviProgetto(Long id);
+	
+	}

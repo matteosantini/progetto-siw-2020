f2c4ea36d9717d6e7d26a4580327332d910fefa1
Davide <Davide@DESKTOP-TINIHFS>
2020-06-04 18:41:46 +0200

    FX

--- siw/src/main/java/it/uniroma3/progetto2020/model/Credentials.java
+++ siw/src/main/java/it/uniroma3/progetto2020/model/Credentials.java
@@ -2,11 +2,13 @@ package it.uniroma3.progetto2020.model;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 
+@Entity
 public class Credentials {
 	
 	public static final String DEFAULT_ROLE="DEFAULT";
